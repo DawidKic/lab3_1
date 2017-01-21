55c0614942f6ff045f31d4572d9f175d6e4d3376
DawidKic <d.kicinski12@gmail.com>
2017-01-21 16:31:59 +0100

    testowanie adnotajci @mock i @injectMocks

--- src/test/java/lab3_1/TestCase1.java
+++ src/test/java/lab3_1/TestCase1.java
@@ -2,6 +2,8 @@ package lab3_1;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -19,21 +21,24 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class TestCase1 {
 	
+	@Mock ClientData clientData2;
+	@InjectMocks InvoiceRequest invoiceRequest2;
+	
 	@Test
 	public void stateTest() {
 		//given
 		InvoiceFactory invoiceFactory = new InvoiceFactory();
 		BookKeeper bookKeeper = new BookKeeper(invoiceFactory);
 		String name = "name";
-		Id id = Id.generate();
-		ClientData clientData = new ClientData(id, name);
+		ClientData clientData = new ClientData(Id.generate(), name);
+	
 		InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
 		Money money = new Money(10);
 		TaxPolicy taxPolicy = Mockito.mock(TaxPolicy.class);
 		Mockito.when(taxPolicy.calculateTax(ProductType.DRUG, money)).thenReturn(new Tax(money, ""));
 		
 		//when
-		Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+		Invoice invoice = bookKeeper.issuance(invoiceRequest2, taxPolicy);
 		
 		//then
 		Assert.assertThat(invoice.getItems().size(), is(equalTo(1)));
